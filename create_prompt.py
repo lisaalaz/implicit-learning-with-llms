cc8372ebc71c6c79fe07fd5a941ab729bea607e9
lisaalaz <lisa.alazraki20@imperial.ac.uk>
2025-01-17 10:38:50 +0000

    added

---
+++ create_prompt.py
@@ -0,0 +1,59 @@
+import sys
+from prompts.examples import icl_examples
+from prompts.instructions import task_instructions
+from prompts.templates import prompt_templates
+
+def create_prompt(strategy: str, task_type: str, examples_origin: str,
+                   question_or_context: str, answer_or_step: str = None) -> str:
+    """Creates a prompt for a current question/context and (optionally) answer
+       or step, given a prompting strategy, task type and dataset.
+
+    Args:
+        strategy: The desidired prompting strategy. Options are 'cot',
+            'cot_plus', 'explicit', 'implicit'.
+        task type: The type of task to solve. Options are 'label_ans',
+            'label_step', 'edit', 'solve'.
+        examples_origin: Dataset of origin of the in-context examples. Options
+            are 'gsm8k', 'asdiv', 'aqua', 'prm800k'.
+        question_or_context: The current dataset question or context (i.e., the
+            question concatenated with a partial answer).
+        answer_or_step: The answer to the current question, or the next
+            reasoning step for the current context.
+                   
+    Returns:
+        The complete prompt for the given arguments, ready to pass to an LLM.
+    """
+    strategy_ops = ['cot', 'cot_plus', 'explicit', 'implicit']
+    task_ops = ['label_ans', 'label_step', 'edit', 'solve']
+    origins = ['gsm8k', 'asdiv', 'aqua', 'prm800k']
+    assert strategy in strategy_ops, "strategy must be one of 'cot', 'cot_plus', 'explicit', 'implicit'."
+    assert task_type in task_ops, "task_type must be one of 'label_ans', 'label_step', 'edit', 'solve'."
+    assert examples_origin in origins, "examples_origin must be one of 'gsm8k', 'asdiv', 'aqua', 'prm800k'."
+    if task_type in ['label_ans', 'label_step', 'edit']:
+        assert answer_or_step is not None, "answer_or_step must be specified for this task."
+
+    examples_id = f"{examples_origin}_{strategy}_examples"
+    task_id = "solve_cot" if (
+                               task_type == "solve" and strategy == "cot"
+                             ) else task_type
+    examples_with_instruction = "{}\n\n\n\n{}".format(
+                                                icl_examples[examples_id],
+                                                task_instructions[task_id]
+                                                )
+    if examples_origin != "prm800k" and "label" not in task_type:
+        examples_with_instruction += " Express the final numerical answer as an integer or floating point number."
+    prompt = prompt_templates[f"{task_id}_template"].format(
+                                                examples_with_instruction,
+                                                question_or_context,
+                                                answer_or_step)
+    return prompt
+
+
+if __name__ == "__main__":
+    print(create_prompt(*sys.argv[1:]))
+
+
+
+
+
+
